7b5c75bcda3fb08ff23991106cf90fe888ea27d4
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-10-07 19:33:05 +0800

    iterationMapping.js

--- iterationMapping.js
+++ iterationMapping.js
@@ -1,1 +1,17 @@
+// iterationMapping.js
+const maxLimit = parseInt(prompt("Enter the Limit of the Double Loop: "));
 
+// Initialize counters
+let i = 0;
+let j = 0;
+
+// Loop through each iteration
+for (i = 0; i < maxLimit; i++) {
+  for (j = 0; j < maxLimit; j++) {
+    // Calculate the added value
+    const addedValue = i + j;
+
+    // Log the iteration points and added value
+    console.log(`[{i}] [{j}] Added value is {addedValue}`);
+  }
+}
